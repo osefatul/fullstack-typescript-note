d2a06074a6ed5917f83de34ecccd6ba7724ee549
Sefatullah Omar <sefaullahomar1@gmail.com>
2023-01-15 23:28:09 -0800

    createda pp

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+# Note App
+
+Commonly, RESTful API is a stateless api and public API that have constraints, however, in our case we will be going against restful API rule and have session to store clients data.
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,15 @@
-const express = require('express');
-const app = express();
-const PORT = 5000
+import app from "./app"
+import env from "./util/validateEnv";
+import mongoose from "mongoose";
+
+
+
+const PORT = env.PORT || 5000
+
+mongoose.set('strictQuery', true);
+console.log("Connected to MongoDB")
+mongoose.connect(env.MONGO_URL).then(
+).catch(err => console.error(err));
 
 
 app.listen(PORT, ()=>{
